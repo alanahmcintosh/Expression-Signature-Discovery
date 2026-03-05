dc7d2a0057b1a2e510ec56eaafbf539097a7eb91
alanahmcintosh <112955629+alanahmcintosh@users.noreply.github.com>
2026-03-05 13:36:17 +0000

    Update __init__.py

--- src/Benchmark_Sigs/simulate/__init__.py
+++ src/Benchmark_Sigs/simulate/__init__.py
@@ -1,13 +1,13 @@
 # src/Benchmark_Sigs/simulate/__init__.py
 from .alterations.simulate_alterations import (
-    simulate_X_hybrid,
+    simulate_X,
     split_simulated_blocks,
 )
 
 from .rna.simulate_rna import simulate_rna_with_signatures
 
 __all__ = [
-    "simulate_X_hybrid",
+    "simulate_X",
     "split_simulated_blocks",
     "simulate_rna_with_signatures",
 ]
