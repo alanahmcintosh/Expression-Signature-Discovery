3955f04c54e053e0550d12e1c79f6de42583556d
alanahmcintosh <112955629+alanahmcintosh@users.noreply.github.com>
2026-03-05 12:47:31 +0000

    Update __init__.py

--- src/Benchmark_Sigs/preprocess/__init__.py
+++ src/Benchmark_Sigs/preprocess/__init__.py
@@ -1,5 +1,32 @@
-__all__ = [
-    "__version__",
-]
+# src/Benchmark_Sigs/preprocess/__init__.py
+from .clinical import (
+    process_subtypes,
+    select_known_clinicals,
+    encode_altertaions_clinical,
+)
 
-__version__ = "0.1.0"
+from .mutations import (
+    classify_variant,
+    maf_to_onehot,
+)
+
+from .integrate import integrate_data
+
+from .RNA import (
+    preprocess_rna_for_simulation,
+    select_genes_with_expr_filter,
+)
+
+from .fusions import read_fusions_file
+
+__all__ = [
+    "process_subtypes",
+    "select_known_clinicals",
+    "encode_altertaions_clinical",
+    "classify_variant",
+    "maf_to_onehot",
+    "integrate_data",
+    "preprocess_rna_for_simulation",
+    "select_genes_with_expr_filter",
+    "read_fusions_file",
+]
