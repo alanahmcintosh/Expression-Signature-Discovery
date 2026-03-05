b3e2cb869de4b4cfe364b0cd5392cbd8e7956663
alanahmcintosh <112955629+alanahmcintosh@users.noreply.github.com>
2026-03-05 12:50:26 +0000

    Update __init__.py

--- src/Benchmark_Sigs/simulate/rna/__init__.py
+++ src/Benchmark_Sigs/simulate/rna/__init__.py
@@ -1,5 +1,19 @@
-__all__ = [
-    "__version__",
-]
+# src/Benchmark_Sigs/simulate/rna/__init__.py
+from .deseq_params import estimate_deseq2_parameters, draw_size_factors_from_deseq
+from .background_rna_knn import simulate_background_from_alterations_knn
+from .signature_params import build_alt_params_from_deseq2_summary
+from .signatures import generate_signatures_from_deseq2_params, induce_expression_effects
+from .nb_sampling import sample_nb, sample_nb_for_signature_genes
+from .simulate_rna import simulate_rna_with_signatures
 
-__version__ = "0.1.0"
+__all__ = [
+    "estimate_deseq2_parameters",
+    "draw_size_factors_from_deseq",
+    "simulate_background_from_alterations_knn",
+    "build_alt_params_from_deseq2_summary",
+    "generate_signatures_from_deseq2_params",
+    "induce_expression_effects",
+    "sample_nb",
+    "sample_nb_for_signature_genes",
+    "simulate_rna_with_signatures",
+]
