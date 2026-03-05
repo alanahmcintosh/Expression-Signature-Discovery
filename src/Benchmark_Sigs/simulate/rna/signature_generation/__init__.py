a9c316ba6eda2cdb1b200475dd858c92b23eda9d
alanahmcintosh <112955629+alanahmcintosh@users.noreply.github.com>
2026-03-05 13:38:04 +0000

    Update __init__.py

--- src/Benchmark_Sigs/simulate/rna/signature_generation/__init__.py
+++ src/Benchmark_Sigs/simulate/rna/signature_generation/__init__.py
@@ -1,5 +1,10 @@
-__all__ = [
-    "__version__",
-]
+# src/Benchmark_Sigs/simulate/rna/signature_generation/__init__.py
 
-__version__ = "0.1.0"
+from .signature_params import build_alt_params_from_deseq2_summary
+from .simulate_signatures import generate_signatures_from_deseq2_params, induce_expression_effects
+
+__all__ = [
+    "build_alt_params_from_deseq2_summary",
+    "generate_signatures_from_deseq2_params",
+    "induce_expression_effects",
+]
