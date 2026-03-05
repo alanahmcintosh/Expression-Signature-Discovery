af04b07d3fa9487311864bdc8b90886d5a798848
alanahmcintosh <112955629+alanahmcintosh@users.noreply.github.com>
2026-03-05 16:13:21 +0000

    Update __init__.py

--- src/benchmark_sigs/methods/supervised/deconfounder/__init__.py
+++ src/benchmark_sigs/methods/supervised/deconfounder/__init__.py
@@ -1,1 +1,14 @@
 
+# methods/deconfounder/__init__.py
+
+from .ppca import ppca, predicitve_check, choose_latent_dim_ppca
+from .outcome_model import deconfounder
+from .pipeline import compute_deconfounder
+
+__all__ = [
+    "ppca",
+    "predicitve_check",
+    "choose_latent_dim_ppca",
+    "deconfounder",
+    "compute_deconfounder",
+]
