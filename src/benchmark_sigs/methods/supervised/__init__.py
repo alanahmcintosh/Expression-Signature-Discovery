005a60e9a77a318a2dcf83ee7fad1f15f0e14e7d
alanahmcintosh <112955629+alanahmcintosh@users.noreply.github.com>
2026-03-05 16:13:01 +0000

    Update __init__.py

--- src/benchmark_sigs/methods/supervised/__init__.py
+++ src/benchmark_sigs/methods/supervised/__init__.py
@@ -1,1 +1,33 @@
 
+# methods/supervised/__init__.py
+
+from .normalise import normalize_counts_log_cpm
+from .feature_selection import select_features_elbow
+
+from .models import (
+    fit_alt_to_expr_weights_lasso,
+    fit_alt_to_expr_weights_elasticnet,
+    fit_alt_to_expr_weights_ridge,
+    fit_alt_to_expr_weights_svm,
+    fit_alt_to_expr_importances_rf,
+)
+
+from .deseq2 import get_deseq2_signature_binary
+
+from .wrapper import (
+    precompute_supervised_weights,
+    create_supervised_signatures,
+)
+
+__all__ = [
+    "normalize_counts_log_cpm",
+    "select_features_elbow",
+    "fit_alt_to_expr_weights_lasso",
+    "fit_alt_to_expr_weights_elasticnet",
+    "fit_alt_to_expr_weights_ridge",
+    "fit_alt_to_expr_weights_svm",
+    "fit_alt_to_expr_importances_rf",
+    "get_deseq2_signature_binary",
+    "precompute_supervised_weights",
+    "create_supervised_signatures",
+]
