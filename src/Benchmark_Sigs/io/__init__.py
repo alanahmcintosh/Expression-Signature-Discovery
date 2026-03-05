e7fa919f75a60342fdd97956cc4840522bc14c66
alanahmcintosh <112955629+alanahmcintosh@users.noreply.github.com>
2026-03-05 13:32:06 +0000

    Update __init__.py

--- src/Benchmark_Sigs/io/__init__.py
+++ src/Benchmark_Sigs/io/__init__.py
@@ -1,14 +1,12 @@
 # src/Benchmark_Sigs/io/__init__.py
 from .readers import (
     read_cna_file,
-    read_fusions_file,
     read_clinical_file,
     read_rna_file,
 )
 
 __all__ = [
     "read_cna_file",
-    "read_fusions_file",
     "read_clinical_file",
     "read_rna_file",
 ]
