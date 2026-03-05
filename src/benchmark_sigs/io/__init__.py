e20c356898ed4fb511e04194998d71a489b45cfb
alanahmcintosh <112955629+alanahmcintosh@users.noreply.github.com>
2026-03-05 13:49:27 +0000

    Update __init__.py

--- src/benchmark_sigs/io/__init__.py
+++ src/benchmark_sigs/io/__init__.py
@@ -4,9 +4,11 @@ from .readers import (
     read_clinical_file,
     read_rna_file,
 )
+from .writers import save_simulation_outputs
 
 __all__ = [
     "read_cna_file",
     "read_clinical_file",
     "read_rna_file",
+    "save_simulation_outputs",
 ]
